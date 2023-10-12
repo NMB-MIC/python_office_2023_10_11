c2c99ff4540ba1006cb9728fd11f70d8227ca25b
suraphop <mic@admin.com>
2023-10-12 09:38:47 +0700

    add content

---
+++ src/calculation.py
@@ -0,0 +1,11 @@
+def calulation_minus(digit_1,ditgit_2):
+    """minus digit"""
+    result = digit_1-ditgit_2
+    #print(result)
+    return result
+
+def calulation_plus(digit_1,ditgit_2):
+    """plus digit"""
+    result = digit_1+ditgit_2
+    #print(result)
+    return result
